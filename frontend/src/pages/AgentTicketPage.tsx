6b199b36f68c99d2ec9356f4bc4d4088e7ac7771
sasipong jiteufeu <115165630+sasipongjiteufeu@users.noreply.github.com>
2025-11-19 10:22:27 +0700

    ...

--- frontend/src/pages/AgentTicketPage.tsx
+++ frontend/src/pages/AgentTicketPage.tsx
@@ -265,6 +265,7 @@ export default function AgentTicketsPage() {
                 active={filter === 'RESOLVED'}
                 onClick={() => setFilter('RESOLVED')}
               />
+              
             </div>
 
             {/* search bar on the right */}
